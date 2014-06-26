c652980c24f0b089433764d8cb25cd329eb0f7ef
alexander <alexander.ressler@gmail.com>
2014-06-25 20:01:26 -0700

    struct for bmp file header and info sections

--- bmp.c
+++ bmp.c
@@ -2,16 +2,52 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+    unsigned short type;  // Magic id
+    unsigned int size;  // byte count of file size
+    unsigned short int reserved1, reserved2;
+    unsigned short int offset; // Number of bytes that offsets the image data block.
+} BMPHEADER;
+
+typedef struct {
+    unsigned int size;               /* Header size in bytes      */
+    int width,height;                /* Width and height of image */
+    unsigned short int planes;       /* Number of colour planes   */
+    unsigned short int bits;         /* Bits per pixel            */
+    unsigned int compression;        /* Compression type          */
+    unsigned int imagesize;          /* Image size in bytes       */
+    int xresolution,yresolution;     /* Pixels per meter          */
+    unsigned int ncolours;           /* Number of colours         */
+    unsigned int importantcolours;   /* Important colours         */
+} BMPINFO;
+
+void writeHead (BMPHEADER *);
+void writeInfo (BMPINFO *);
+
+void writeHead (BMPHEADER * head)
+{
+    head->type = 0x4D42;
+    head->size = 0x0;
+    head->reserved1 = 0x0;
+    head->reserved2 = 0x0;
+    head->offset = 14;
+    return;
+}
+
+
 
 int main (int argc, char *argv[]) 
 {
     FILE *f;
     unsigned char *img = NULL;
 
+    BMPHEADER head; 
+    BMPINFO info;
 
+    writeHead( &head );
 
-    int w = 100;
-    int h = 100;
+    int w = 768;
+    int h = 768; 
 
     int red[w][h];
     int green[w][h];
@@ -23,33 +59,33 @@ int main (int argc, char *argv[])
     int filesize = 54 + 3*w*h;  //w is your image width, h is image height, both int
     int i,j;
 
+    printf("\n\n");
 
+    printf("Initializing rgb.\n");
     for (i=0; i<w; i++) {
         for (j=0; j<h; j++) {
-            red[i][j] = i;
+            red[i][j] = 0 % 255;
+            blue[i][j] = i % 255;
+            green[i][j] = j % 255;
         }
     }
 
-
-
-
-    if ( img ) { free( img ); }
+    //if ( img ) { free( img ); }
 
     img = (unsigned char *) malloc(3*w*h);
-    memset(img,0,sizeof(img));
+
+    //memset(img, 0, sizeof(img) );
+    //printf("%d\n\n", sizeof(img));
 
     for(i=0; i<w; i++) {
         for(j=0; j<h; j++) {
             x=i; y=(h-1)-j;
             //y=(yres-1)-j;
 
-            r = red[i][j]*255; 
-            g = green[i][j]*255;
-            b = blue[i][j]*255;
+            r = red[i][j];
+            g = green[i][j];
+            b = blue[i][j];
 
-            if (r > 255) r=255;
-            if (g > 255) g=255;
-            if (b > 255) b=255;
             img[(x+y*w)*3+2] = (unsigned char)(r);
             img[(x+y*w)*3+1] = (unsigned char)(g);
             img[(x+y*w)*3+0] = (unsigned char)(b);
@@ -60,11 +96,13 @@ int main (int argc, char *argv[])
     unsigned char bmpinfoheader[40] = {40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0, 24,0};
     unsigned char bmppad[3] = {0,0,0};
 
+    printf("creating the file headers...\n");
     bmpfileheader[ 2] = (unsigned char)(filesize    );
     bmpfileheader[ 3] = (unsigned char)(filesize>> 8);
     bmpfileheader[ 4] = (unsigned char)(filesize>>16);
     bmpfileheader[ 5] = (unsigned char)(filesize>>24);
 
+    printf("creating the file info headers...\n");
     bmpinfoheader[ 4] = (unsigned char)( w     );
     bmpinfoheader[ 5] = (unsigned char)( w>> 8 );
     bmpinfoheader[ 6] = (unsigned char)( w>>16 );
@@ -74,6 +112,8 @@ int main (int argc, char *argv[])
     bmpinfoheader[10] = (unsigned char)( h>>16 );
     bmpinfoheader[11] = (unsigned char)( h>>24 );
 
+    printf("THE INFO HEADER  is %s\n\n", bmpinfoheader);
+    printf("Opening file for writing in binary mode.\n");
     f = fopen("img.bmp","wb");
     fwrite(bmpfileheader,1,14,f);
     fwrite(bmpinfoheader,1,40,f);
@@ -83,6 +123,7 @@ int main (int argc, char *argv[])
         fwrite(bmppad,1,(4-(w*3)%4)%4,f);
     }
 
+    printf("Finished.\n\n");
     fclose(f);
     free(img);
     return 0;
