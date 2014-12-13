581519a7f427af6520f2a3361577bcc559b64036
aressler38 <alexander.ressler@gmail.com>
2014-12-13 15:52:42 -0800

    use multithreading for faster image processing

--- mandelbrot/c++/mandelbrot.cpp
+++ mandelbrot/c++/mandelbrot.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 #include "mandelbrot.h"
+#include <thread>
 
-#define DOTS 1200
-#define MAX_ITER 1000
+#define DOTS 16324
 
-/**
- * Create a fractal image to be read by libpng
- */
-mandelbrot::mandelbrot(double xmin, double ymax, double xmax, double ymin) 
-  : boundingbox{xmin, ymax, xmax, ymin}, imgData(matrix<double>(DOTS,DOTS))
-{
+unsigned mandelbrot::MAX_ITER = 1024;
+
+void top_thread (mandelbrot* m) {
+  unsigned dots = DOTS/2;
+  double xmin = m->boundingbox[0];
+  double ymax = m->boundingbox[1];
+  double xmax = m->boundingbox[2];
+  double ymin = m->boundingbox[3];
 
   double xdelta = (xmax - xmin) / DOTS;
   double ydelta = (ymax - ymin) / DOTS;
 
   for ( unsigned i=0; i<DOTS; ++i ) {
-    for ( unsigned j=0; j<DOTS; ++j ) {
+    for ( unsigned j=0; j<dots; ++j ) {
       double xc = xmin + i*xdelta;
       double yc = ymin + j*ydelta;
-      double x0 = xmin + i*xdelta;
-      double y0 = ymin + j*ydelta;
+      double x0 = xc;
+      double y0 = yc;
       double x = x0*x0 - y0*y0 + xc;
       double y = 2.0*x0*y0 + yc;
       double area = 0.0;
@@ -31,12 +33,90 @@ mandelbrot::mandelbrot(double xmin, double ymax, double xmax, double ymin)
         x = x0*x0 - y0*y0 + xc;
         y = 2.0*x0*y0 + yc;
         ++iter;
-      } while ( area < 4.0 && iter < MAX_ITER );
-      imgData(i,j) = area;
-      //std::cout << "PROCESSED: ("
-      //<< xc << ", " << yc << ") :: " << area << " iter: " << iter <<"\n";
+      } while ( area < 4.0 && iter < mandelbrot::MAX_ITER );
+      m->imgData(i,j) = mandelbrot::img_datum(area, iter);
     }
   }
+
+}
+
+void bottom_thread (mandelbrot* m) {
+  unsigned dots = DOTS/2;
+  double xmin = m->boundingbox[0];
+  double ymax = m->boundingbox[1];
+  double xmax = m->boundingbox[2];
+  double ymin = m->boundingbox[3];
+
+  double xdelta = (xmax - xmin) / DOTS;
+  double ydelta = (ymax - ymin) / DOTS;
+
+  for ( unsigned i=0; i<DOTS; ++i ) {
+    for ( unsigned j=dots; j<DOTS; ++j ) {
+      double xc = xmin + i*xdelta;
+      double yc = ymin + j*ydelta;
+      double x0 = xc;
+      double y0 = yc;
+      double x = x0*x0 - y0*y0 + xc;
+      double y = 2.0*x0*y0 + yc;
+      double area = 0.0;
+      unsigned iter = 0;
+      do {
+        area = x*x + y*y;
+        x0 = x;
+        y0 = y;
+        x = x0*x0 - y0*y0 + xc;
+        y = 2.0*x0*y0 + yc;
+        ++iter;
+      } while ( area < 4.0 && iter < mandelbrot::MAX_ITER );
+      m->imgData(i,j) = mandelbrot::img_datum(area, iter);
+    }
+  }
+
+}
+
+/**
+ * Create a fractal image to be read by libpng
+ */
+mandelbrot::mandelbrot(double xmin, double ymax, double xmax, double ymin) 
+  : boundingbox{xmin, ymax, xmax, ymin}, 
+    imgData(matrix<img_datum>(DOTS,DOTS))
+{
+
+  if ( ! true ) {
+    unsigned dots = DOTS;
+    double xdelta = (xmax - xmin) / dots;
+    double ydelta = (ymax - ymin) / dots;
+
+    for ( unsigned i=0; i<DOTS; ++i ) {
+      for ( unsigned j=0; j<DOTS; ++j ) {
+        double xc = xmin + i*xdelta;
+        double yc = ymin + j*ydelta;
+        double x0 = xc;
+        double y0 = yc;
+        double x = x0*x0 - y0*y0 + xc;
+        double y = 2.0*x0*y0 + yc;
+        double area = 0.0;
+        unsigned iter = 0;
+        do {
+          area = x*x + y*y;
+          x0 = x;
+          y0 = y;
+          x = x0*x0 - y0*y0 + xc;
+          y = 2.0*x0*y0 + yc;
+          ++iter;
+        } while ( area < 4.0 && iter < MAX_ITER );
+        imgData(i,j) = mandelbrot::img_datum(area, iter);
+      }
+    }
+  } else {
+    std::cout << "Creating threads...\n";
+    std::thread t1(top_thread, this);
+    std::thread t2(bottom_thread, this);
+    std::cout << "Joining threads...\n";
+    t1.join();
+    t2.join();
+  }
+  std::cout << "DONE\n" << std::endl;
 
 }
 
--- mandelbrot/c++/mandelbrot.h
+++ mandelbrot/c++/mandelbrot.h
@@ -4,17 +4,30 @@
 #include "matrix.h"
 
 class mandelbrot {
-  typedef unsigned u_int;
-  typedef double _cordinate_box_[4];
+
+  private:
+    typedef unsigned u_int;
+    typedef double _cordinate_box_[4];
+
   public:
-    mandelbrot(double xmin=-2.5, double ymax=1.25, double xmax=1.0, double ymin=-1.25);
+    mandelbrot(double xmin=-2.5, double ymax=1.25, double xmax=1.0, double ymin=-1.25) ;
+    //mandelbrot(double xmin=-0.25, double ymax=-1.0, double xmax=0.25, double ymin=-0.77) ;
+    //mandelbrot(double xmin=-0.25, double ymax=-1.0, double xmax=0.0, double ymin=-0.642857) ;
+
     ~mandelbrot ();
+
+    typedef struct img_datum {
+      img_datum(double a=-1.0, unsigned i=-1) : area(a), iter(i) { }
+      double area;
+      unsigned iter;
+    } img_datum;
     unsigned get_pixel_width() const;
     unsigned get_pixel_height() const;
-    matrix<double> imgData;
-  private:
+    matrix<img_datum> imgData;
+    static unsigned MAX_ITER;
     _cordinate_box_ boundingbox;
+
 };
 
-#endif
 
+#endif
