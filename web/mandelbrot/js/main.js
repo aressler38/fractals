47791701f78881779c333774eb32df3bc0402c81
aressler38 <alexander.ressler@gmail.com>
2017-01-30 09:12:56 +0000

    better colors, fixed inverted y

--- web/mandelbrot/js/main.js
+++ web/mandelbrot/js/main.js
@@ -36,6 +36,7 @@ define([
 		});
 
 		function workerReady() {
+			window.onerror = function (e) {alert(e);};
 			initUI();
 			getMandelbrot();
 		}
@@ -220,30 +221,36 @@ define([
 	}
 
 	function reset() {
-		config.x = [-2.5, 1.0];
-		config.y = [-1.25, 1.25];
+		config.x = [-3.5, 2.0];
+		config.y = [-2.25, 2.25];
 		config.width = window.innerWidth;
 		config.height = window.innerHeight;
 		config.heightWidthRatio = config.height / config.width;
 		canvas.width = config.width;
 		canvas.height = config.height;
+		var width = Math.abs(config.x[0] - config.x[1]),
+			height = width * config.heightWidthRatio,
+			diff = Math.abs(config.y[0] - config.y[1]) - height;
+
+		config.y[1] -= diff/2;
+		config.y[0] += diff/2;
 	}
 
 	function getRectCoords() {
 		var rectClientRect = rect.getBoundingClientRect();
 		var canvasClientRect = canvas.getBoundingClientRect();
 		var xOff = rectClientRect.left - canvasClientRect.left;
-		var yOff = rectClientRect.top - canvasClientRect.top;
-		var dx = (config.x[1] / config.width)	-	(config.x[0]	/ config.width) ;
-		var dy = (config.y[1] / config.height)	-	(config.y[0]	/ config.height) ;
+		var yOff = Math.abs(rectClientRect.bottom - canvasClientRect.bottom);
+		var dx = (config.x[1] / config.width)	-	(config.x[0]	/ config.width); 
+		var dy = (config.y[1] / config.height)	-	(config.y[0]	/ config.height);
 		var x = [null, null];
 		var y = [null, null];
 
-		x[0] = config.x[0] + xOff*dx ;
-		x[1] = x[0] + rectClientRect.width*dx ;
+		x[0] = config.x[0] + xOff*dx;
+		x[1] = x[0] + rectClientRect.width*dx;
 
-		y[0] = config.y[0] + yOff*dy ;
-		y[1] = y[0] + rectClientRect.height*dx ;
+		y[0] = config.y[0] + yOff*dy;
+		y[1] = y[0] + rectClientRect.height*dx;
 
 		console.debug(config);
 		console.debug([x,y].map(function(coords) {
@@ -252,18 +259,20 @@ define([
 
 		return {
 			x : x,
-			y : y
+			y : y,
 		}
 	}
 
-	function buildImage(data) {
-		var bytes = new Uint8Array(data.buffer);
-		var image = context.createImageData(canvas.width, canvas.height);
-		for ( var i=0; i<bytes.length; ++i ) {
+	function buildImage (data) {
+		var bytes = new Uint8Array(data.buffer),
+			image = context.createImageData(canvas.width, canvas.height),
+			i;
+
+		for (i=0; i<bytes.length; ++i) {
 			image.data[i] = bytes[i];
 		}
 		context.putImageData(image, 0, 0);
-		pacman.style.display='none';
+		pacman.style.display = 'none';
 	}
 
 	function requestFullScreen () {
@@ -278,8 +287,10 @@ define([
 		} else if (el.webkitRequestFullscreen) {
 			el.webkitRequestFullscreen();
 		}
-		reset();
-		getMandelbrot();
+		setTimeout(function () {
+			reset();
+			getMandelbrot();
+		}, 1000);
 	}
 
 	function getMandelbrot () {
--- web/mandelbrot/js/mandelbrot.js
+++ web/mandelbrot/js/mandelbrot.js
@@ -1,58 +1,64 @@
-define(function() {
+define(function () {
 
     /**
      * @param {Object} userConfig  
      * @returns {ArrayBuffer} buffer This is a pixel array representing a
      *      32-bit image with dimensions (width x height).
      */
-    function mandelbrot(userConfig) {
+    function mandelbrot (userConfig) {
       var config = {
         // cartesian coordinates for extrema
         x: [-2.5, 1.0],
         y: [-1.25, 1.25],
         // pixel width x height
         width: 800,
-        height: 600
-      }
-      for (var param in userConfig) {
+        height: 600,
+      },
+			param;
+
+      for (param in userConfig) {
         config[param] = userConfig[param];
       }
 
       var buffer = null;
       var bufferView = null;
-      var i, j; // counters
+      var i, j, idy, iwidth;
       var x, y, x0, y0; // computed coordinates
       var xtemp;
       var offset = 0; // keep track of the byte offset in the loops
       var iteration = 0;
       var totalIterations = 0;
-      const width = parseInt(config.width);
-      const height = parseInt(config.height);
-      const X = Math.abs(config.x[1] - config.x[0]); // Length
-      const Y = Math.abs(config.y[1] - config.y[0]); // Length
-      const dx = X/width; 
-      const dy = Y/height; 
-      const xmin = Math.min.apply(this, config.x);
-      const ymin = Math.min.apply(this, config.y);
-      const xmax = xmin + X;
-      const ymax = ymin + Y;
-      const bitmapSize = width * height * 4; // 32 bit img
-      const MAX_ITERATION = 1000;
+			// consts 
+      var width = parseInt(config.width);
+      var height = parseInt(config.height);
+      var X = Math.abs(config.x[1] - config.x[0]); // Length
+      var Y = Math.abs(config.y[1] - config.y[0]); // Length
+      var dx = X/width; 
+      var dy = Y/height; 
+      var xmin = Math.min.apply(null, config.x);
+      var ymin = Math.min.apply(null, config.y);
+      var xmax = xmin + X;
+      var ymax = ymin + Y;
+      var bitmapSize = width * height * 4; // 32 bit img
+      var MAX_ITERATION = 1024;
 
       buffer = new ArrayBuffer(bitmapSize);
       bufferView = new DataView(buffer);
 
       // Write the pixel array
-      for ( i=0; i<height; ++i ) {          // for each row, 
-        for ( j=0; j<width; ++j ) {       // iterate over each column.
-          offset = 4*(i*width + j);   // byte offset
-          y0 = ymin + i*dy;
+      for (i=0; i<height; ++i) {          // for each row, 
+				idy = i*dy;
+				iwidth = i*width
+
+        for (j=0; j<width; ++j) {       // iterate over each column.
+          offset = 4*(iwidth + j);   // byte offset
+          y0 = ymax - idy;
           x0 = xmin + j*dx;
           x  = 0.0;
           y  = 0.0;
           iteration = 0;
           
-          while ( x*x + y*y < 4 && iteration < MAX_ITERATION ) {
+          while (x*x + y*y < 4 && iteration < MAX_ITERATION) {
             xtemp = x*x - y*y + x0;
             y = 2*x*y + y0;
             x = xtemp;
@@ -64,34 +70,21 @@ define(function() {
         }
       }
 
+			// color is rgba => 0xrrggbbaa
       function palette (iteration) {
-        var color ;
-        color = (iteration === MAX_ITERATION) ? 0x000000ff : 
-          (iteration > 900) ? 0x0000ffff : 
-          (iteration > 800) ? 0xf8f808ff :
-          (iteration > 700) ? 0xf7f707ff : 
-          (iteration > 600) ? 0xf6f6f6ff : 
-          (iteration > 500) ? 0xf5f5f5ff : 
-          (iteration > 400) ? 0xf4f4f4ff : 
-          (iteration > 300) ? 0xf3f3f3ff : 
-          (iteration > 200) ? 0xf10202ff : 
-          (iteration > 100) ? 0xf10101ff : 
-          (iteration > 50)  ? 0xaeaeaeae : 
-          (iteration > 40)  ? 0xadadadff : 
-          (iteration > 30)  ? 0xa0a0a0ff : 
-          (iteration > 20)  ? 0x505050ff : 
-          (iteration > 10)  ? 0x0222afff : 
-          (iteration > 9)   ? 0x002aafff : 
-          (iteration > 8)   ? 0x00ffbeff : 
-          (iteration > 7)   ? 0x0030beff : 
-          (iteration > 5)   ? 0x0022aaff : 
-          (iteration > 3)   ? 0x0000bbff : 
-          (iteration > 2)   ? 0x0000ffff : 
-          (iteration > 1)   ? 0xffffffff : 0xffffffff; 
-        return 0xffffffff & color;
+        var black = 0x000000ff,
+					color = ((iteration % 0xff) << 24) +
+						((iteration % 0x33) << 16) +
+						((iteration % 0x66) << 8) +
+						black;
+
+        return color;
       }
       
-      return { buffer:buffer, iterations:totalIterations };
+      return {
+				buffer:buffer,
+				iterations:totalIterations,
+			};
     }
 
     return mandelbrot;
